03f0b81d6ac989c764377eba32346aebb5595b0b
Elder Xavier <eldersxavier@gmail.com>
2015-07-27 12:29:04 -0300

    incluido faviico

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <link rel="shortcut icon" href="imagens/favicon.png">
-  <link rel="icon" href="imagens/favicon.png" type="image/x-icon">
+  <link rel="icon" type="image/png" href="imagens/favico.png">  
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
   <meta name="HandheldFriendly" content="true"/>
   <link rel="stylesheet" href="css/stylesheets/screen.css">
